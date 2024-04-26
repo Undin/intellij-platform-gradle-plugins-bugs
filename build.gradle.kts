57c58c309104a282b1f4e24f7f2a56f1303d54e9
Arseniy Pendryak <arseniy.pendryak@gmail.com>
2024-04-26 18:53:22 +0200

    `SandboxAware` task example

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.intellij.platform.gradle.tasks.aware.SandboxAware
+
 plugins {
     kotlin("jvm") version "1.9.23"
     id("org.jetbrains.intellij.platform") version "2.0.0-SNAPSHOT"
@@ -31,4 +33,10 @@ dependencies {
         intellijIdeaUltimate("2024.1")
     }
     testImplementation(kotlin("test"))
+}
+
+abstract class MergePluginJarsTask : Jar(), SandboxAware
+
+tasks {
+    val mergePluginJarTask by registering(MergePluginJarsTask::class) {}
 }
